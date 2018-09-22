9f0b16e7349f1e3e84d310d9088b634d07273d37
Alec Roques <alec.roques@gmail.com>
2018-09-22 18:43:15 -0500

    Add bias

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-from math import log, e, pow
+from math import e, pow, log
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 
 
 def main():
+    x_range = np.array(range(-10, 10))
+    lr = LogisticRegression()
+
     data = np.genfromtxt('training_data.csv', delimiter=',', dtype='int32')
     x, y = data[:, 0], data[:, 1]
+    x = np.column_stack((np.ones_like(x), x))  # Let x0 equal 1
+
+    lr.fit(x, y)
+
+    print(x)
+    print(y)
 
     eta = 0.1
 
-    w = np.random.uniform(-1, 1, 1)
+    w = np.random.uniform(-10, 10, 2)
 
-    for this_x in x:
-        pred = logistic_fn(w, this_x)
-        print(pred)
-    print()
-    t = 100000
+    t = 200000
 
-    for _ in range(t):
+    for i in range(t):
         g = calculate_gradient(w, x, y)
         w = w - eta * g
-        if g < 0.000001:
+
+        if np.sum(np.absolute(g)) < 0.0001 and i > 10000:
+            print()
+            print(i)
             break
 
-    for this_x in x:
-        pred = logistic_fn(w, this_x)
+    print()
+
+    for this_x, this_y in zip(x, y):
+        pred = logistic_fn(w, this_x, this_y)
         print(pred)
 
 
@@ -31,16 +43,27 @@ def calculate_gradient(w, x, y):
     gradient_sum = 0
     for this_x, this_y in zip(x, y):
         gradient_sum += partial_gradient(w, this_x, this_y)
-    return - (gradient_sum / len(x))
+    return - (gradient_sum / x.shape[0])
 
 
 def partial_gradient(w, x, y):
     return (y * x) / (1 + pow(e, y * (np.dot(w, x))))
 
 
-def logistic_fn(w, x):
-    s = w * x
+def logistic_fn(w, x, y):
+    s = y * np.dot(w, x)
     return pow(e, s) / (1 + pow(e, s))
+
+
+def insample_error(w, x, y):
+    sum = 0
+    for this_x, this_y in zip(x, y):
+        sum += pt_error(w, this_x, this_y)
+    return sum / x.shape[0]
+
+
+def pt_error(w, x, y):
+    return log(1 + pow(e, -y * np.dot(w, x)))
 
 
 if __name__ == '__main__':
